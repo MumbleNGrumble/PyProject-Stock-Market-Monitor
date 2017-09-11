5eb7d8fcbee0859580189d015ac7d8e6d5a236f4
MumbleNGrumble <le.daniel90@gmail.com>
2017-09-10 21:18:48 -0700

    Added GetRecentData function

--- stock_market_monitor.py
+++ stock_market_monitor.py
@@ -22,6 +22,51 @@ def GetHistoricalData(ticker, source):
     # Yahoo dataframe columns: Open, High, Low, Close, Adj Close, and Volume.
     return web.DataReader(ticker, source, start, end)
 
+def GetRecentData(database, table, ticker, source):
+    '''
+    database (str): Name of the database to read from.
+    table (str): Name of the table to read from.
+    ticker (str): Stock ticker symbol.
+    source (str): The database to pull the data from as outlined in pandas datareader.
+
+    Checks the database for the last stock price record.
+    If more recent data exists, pull the data from the specified source.
+    Returns a dataframe if there are new records. Otherwise, returns None.
+    '''
+    # Get the StockID for the specified stock ticker.
+    stockIDQuery = "SELECT [StockID] FROM [Stock] WHERE [StockTicker] = " + "'" + ticker + "'"
+    stockID = ReadDBToDF(database, stockIDQuery)["StockID"][0]
+
+    # Set up the SQL query to get the latest record date from the database for that stock.
+    selectArg = "SELECT TOP (1) [Date]"
+    fromArg = "FROM [" + table + "]"
+    whereArg = "WHERE [StockID] = " + str(stockID)
+    orderByArg = "ORDER BY [Date] DESC"
+
+    dateQuery = "\n".join([selectArg, fromArg, whereArg, orderByArg])
+
+    # Execute query.
+    latestDate = ReadDBToDF(database, dateQuery, ["Date"])["Date"][0]
+    latestDate = latestDate.date()  # Pandas returns a timestamp. This converts it to a date.
+
+    # TODO: Can we make this more efficient?
+    # TODO: Need to account for errors when the source returns something unexpected.
+    # TODO: Should this write to the database if there is new data?
+    # Returns a dataframe with new records if there is new data between the last database record date and yesterday's date.
+    if latestDate < dt.date.today() - dt.timedelta(days=1):
+        start = latestDate + dt.timedelta(days=1)
+        end = dt.date.today() - dt.timedelta(days=1)
+        df = web.DataReader(ticker, source, start, end)
+
+        if latestDate == df.index[0].date():
+            print("Database is up to date for " + ticker + ".")
+            return None
+        else:
+            return df
+    else:
+        print("Database is up to date for " + ticker + ".")
+        return None
+
 def ReadDBToDF(database, sql, parse_dates=None):
     '''
     database (str): Name of the database to read from.
